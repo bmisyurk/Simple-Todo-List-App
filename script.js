fef9e8536afea5d4ac3917f6143457c1067e0877
Bogdan MISYURKO <bmisyurk@e1r1p14.unit.ua>
2019-05-04 18:09:20 +0300

    local_storage

---
+++ script.js
@@ -0,0 +1,40 @@
+function removeTask(el) {
+        let removeEl = el.parentNode,
+            removeElId = removeEl.id,
+            removeElState = removeEl.classList.contains('app__list-item--done');
+
+        removeEl.remove();
+        const items = removeElState ? tasks.done : tasks.current;
+        for (const [index, item] of items.entries()) {
+            if (item.taskId !== removeElId) continue;
+            items.splice(index, 1);
+        }
+        allTasks.innerHTML = tasks.allTasks;
+        doneTasks.innerHTML = tasks.doneTasks;
+    }
+
+    function addTasks(str) {
+        let elem = {
+            taskId: doId(),
+            taskContent: str,
+            taskState: "current"
+        };
+        tasks.current.push(elem);
+        createItem(elem);
+        allTasks.innerHTML = tasks.allTasks;
+    }
+
+    function doId() {
+        return Math.random().toString(36).substr(2, 16);
+    }
+
+    INIT();
+
+    addNewTaskField.addEventListener('keyup',function (e) {
+        if(e.keyCode === 13) {
+            addTasks(this.value);
+            this.value = "";
+        }
+    })
+
+})();
